6720f457787684951c6d519f8fa8fa45f8ace002
shaikzaid <shaikzaid690@gmail.com>
2022-11-14 11:25:49 +0530

    LightBox (Zaid Edit)

--- Consumer/src/main/java/com/lightbox/consumer/Application.java
+++ Consumer/src/main/java/com/lightbox/consumer/Application.java
@@ -1,14 +1,15 @@
 package com.lightbox.consumer;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication(scanBasePackages = {"com.lightbox"})
-public class Application {
-
-	public static void main(String[] args) {
+public class Application { public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 
 }
